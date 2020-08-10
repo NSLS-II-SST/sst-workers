b3539018cdcb6c73867ce2bb7b1e97c1bceb62b6
jklynch <jlynch@bnl.gov>
2020-08-09 21:30:37 -0400

    reformat

--- analysis_db.py
+++ analysis_db.py
@@ -1,17 +1,12 @@
 config = {
-    'metadatastore': {
-        'module': 'databroker.headersource.mongo',
-        'class': 'MDS',
-        'config': {
-            'directory': 'some_directory',
-            'timezone': 'US/Eastern'}
+    "metadatastore": {
+        "module": "databroker.headersource.mongo",
+        "class": "MDS",
+        "config": {"directory": "some_directory", "timezone": "US/Eastern"},
     },
-    'assets': {
-        'module': 'databroker.assets.sqlite',
-        'class': 'Registry',
-        'config': {
-            'dbpath': assets_dir + '/database.sql'}
-    }
+    "assets": {
+        "module": "databroker.assets.sqlite",
+        "class": "Registry",
+        "config": {"dbpath": assets_dir + "/database.sql"},
+    },
 }
-
-
